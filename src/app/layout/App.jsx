88afdbc3528278e276dddeee5027b4540562b7a6
Anne Cahalan <Anne@Annes-MacBook-Pro.local>
2018-09-26 17:48:12 -0400

    initial commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  // Components are required to have a render() function
+  render() {
+    return (
+      // this is JSX, html-looking business that gets compiled into JS and then is injected into the html page 
+      <div>
+        <h1>Re-vents</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+// ReactDOM vs. React Native, for example 
+// React is now several libraries 
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './app/layout/App';
+import registerServiceWorker from './registerServiceWorker';
+
+const rootEl = document.getElementById('root');
+
+let render = () => { 
+    ReactDOM.render(<App />, rootEl)
+}
+
+// hot module replacement
+// mostly works most of the time 
+if (module.hot) {
+    module.hot.accept('./app/layout/App', () => {
+        setTimeout(render)
+    })
+}
+
+render(); 
+registerServiceWorker();
