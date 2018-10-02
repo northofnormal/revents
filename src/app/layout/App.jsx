ac4607cee8472fbb200fe9d82671932fee46bf68
Anne Cahalan <Anne@Annes-MBP.lan>
2018-10-01 20:24:33 -0400

    End Of Section 3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react'
+import EventDashboard from '../../features/events/EventDashboard/EventDashboard'
+import NavBar from '../../features/nav/NavBar/NavBar'
 
 class App extends Component {
   // Components are required to have a render() function
   render() {
     return (
       // this is JSX, html-looking business that gets compiled into JS and then is injected into the html page 
+      // have to wrap elements in a div for some reason? it's a .jsx thing 
       <div>
-        <h1>Re-vents</h1>
+        <NavBar/>
+      <Container className="main">
+        <EventDashboard/>
+      </Container>  
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 // ReactDOM vs. React Native, for example 
 // React is now several libraries 
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layout/App';
 import registerServiceWorker from './registerServiceWorker';
